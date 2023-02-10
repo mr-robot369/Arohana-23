fb2bf3025b9df2ed7d9fe82097ab5106e9e8bbb8
Anubhav <anubhavshukla870@gmail.com>
2023-02-11 00:17:28 +0530

    sec1

--- Javascript/index.js
+++ Javascript/index.js
@@ -9,3 +9,16 @@ const toggleNavbar = () => {
 };
 
 mobile_nav.addEventListener("click",() => toggleNavbar());
+
+// logo click
+const logo = document.querySelector(".AAROHNA-logo");
+const logo_clicked = () => {
+    // logo.style.pointer = "cursor";
+    if (window.location.href.includes('events')){
+        window.location.href = "../index.html";
+    }
+    else{
+        window.location.href = "./index.html";
+    }
+}
+logo.addEventListener("click",() => logo_clicked());
